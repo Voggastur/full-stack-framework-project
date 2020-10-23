8f50aff03a280012da0ace70f44dcd634c224285
Voggastur <v3x0r_x014@hotmail.com>
2020-10-23 22:59:28 +0000

    removed street2 field reference

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,7 +6,7 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
-                  'street_address1', 'street_address2',
+                  'street_address',
                   'town_or_city', 'postcode', 'country',
                   'county',)
 
@@ -22,8 +22,7 @@ class OrderForm(forms.ModelForm):
             'phone_number': 'Phone Number',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
-            'street_address1': 'Street Address 1',
-            'street_address2': 'Street Address 2',
+            'street_address': 'Street Address',
             'county': 'County, State or Locality',
         }
 

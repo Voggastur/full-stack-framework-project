1c99a0e870d2adc4c8df74d7bd94b3c506fa7489
Voggastur <v3x0r_x014@hotmail.com>
2020-10-28 23:11:40 +0000

    added questions, answers, qform variables to faq templates, to allow for question form fillout in faq page

--- faq/views.py
+++ faq/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from .models import Question, Answer
+from .forms import QuestionForm
 
 # Create your views here.
 
 
 def faq(request):
-    """ A view to return the about page """
+    """ Returns the Frequently Asked Questions template and
+    show the Question Form if user wish to POST """
 
-    return render(request, 'faq/faq.html')
+    questions = Question.objects.all()
+    answers = Answer.objects.all()
+    qform = QuestionForm()
+
+    context = {
+        'questions': questions,
+        'answers': answers,
+        'qform': qform,
+    }
+
+    return render(request, 'faq/faq.html', context)

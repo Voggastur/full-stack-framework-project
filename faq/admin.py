ee7c9915b812465abcdb082e276d09a8ccc3288d
Voggastur <v3x0r_x014@hotmail.com>
2020-10-28 23:16:10 +0000

    added questions and answers for admin page

--- faq/admin.py
+++ faq/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Question, Answer
 
 # Register your models here.
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        'text',
+        'created_at',
+        'created_by',
+    )
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        'text',
+        'created_at',
+        'related_question',
+    )
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)

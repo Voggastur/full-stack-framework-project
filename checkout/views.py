160ee3578046f5c6835ab18979a1e21ab1e3f767
Voggastur <v3x0r_x014@hotmail.com>
2020-10-28 23:40:14 +0000

    removed reference to street adress2

--- checkout/views.py
+++ checkout/views.py
@@ -43,8 +43,7 @@ def checkout(request):
             'country': request.POST['country'],
             'postcode': request.POST['postcode'],
             'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
+            'street_address': request.POST['street_address'],
             'county': request.POST['county'],
         }
 
@@ -115,8 +114,7 @@ def checkout(request):
                     'country': profile.default_country,
                     'postcode': profile.default_postcode,
                     'town_or_city': profile.default_town_or_city,
-                    'street_address1': profile.default_street_address1,
-                    'street_address2': profile.default_street_address2,
+                    'street_address': profile.default_street_address,
                     'county': profile.default_county,
                 })
             except UserProfile.DoesNotExist:
@@ -158,8 +156,7 @@ def checkout_success(request, order_number):
                 'default_country': order.country,
                 'default_postcode': order.postcode,
                 'default_town_or_city': order.town_or_city,
-                'default_street_address1': order.street_address1,
-                'default_street_address2': order.street_address2,
+                'default_street_address': order.street_address,
                 'default_county': order.county,
             }
             user_profile_form = UserProfileForm(profile_data, instance=profile)

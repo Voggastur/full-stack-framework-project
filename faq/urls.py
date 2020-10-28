9937bba6567620db1f0725b5a5c1a34c8aa0bc28
Voggastur <v3x0r_x014@hotmail.com>
2020-10-28 23:19:41 +0000

    added add_question, edit_question and delete_question urls for handlers in faq.html

--- faq/urls.py
+++ faq/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from faq import views
 
 urlpatterns = [
-    path('', views.faq, name='faq')
+    path('', views.faq, name='faq'),
+    path('add_question', views.add_question, name='add_question'),
+    path('edit/<int:question_id>/', views.edit_question, name='edit_question'),
+    path('delete/<int:question_id>/', views.delete_question, name='delete_question'),
 ]

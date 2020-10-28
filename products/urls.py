1fc22fe91a9b589fc09c259086fb893e42109456
Voggastur <v3x0r_x014@hotmail.com>
2020-10-28 23:57:42 +0000

    add urls for add_review, edit_review and delete_review

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:product_id>/', views.product_detail, name='product_detail')
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('add_review/<int:product_id>/', views.add_review, name='add_review'),
+    path('edit/<int:review_id>/',
+         views.edit_review, name='edit_review'),
+    path('delete/<int:review_id>/',
+         views.delete_review, name="delete_review"),
 ]

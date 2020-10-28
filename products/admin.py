9f224f3b2b6e2a88e701fd3b6cda7a2d62475af2
Voggastur <v3x0r_x014@hotmail.com>
2020-10-28 23:56:49 +0000

    add review to admin page

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Review
 
 # Register your models here.
 
@@ -24,5 +24,16 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'header',
+        'body',
+        'reviewed_product',
+        'created_at',
+        'created_by',
+    )
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Review, ReviewAdmin)

8b034b723d54cfd5ec0a9622e8f3efb55bf40274
Voggastur <v3x0r_x014@hotmail.com>
2020-10-29 00:28:02 +0000

    add url for contact view

--- about/urls.py
+++ about/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about, name='about')
+    path('', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]

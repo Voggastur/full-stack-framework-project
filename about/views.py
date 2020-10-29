b6581e53b94553b8b736e2c1fff953fe70fd606c
Voggastur <v3x0r_x014@hotmail.com>
2020-10-29 00:26:57 +0000

    added view to render contact page

--- about/views.py
+++ about/views.py
@@ -7,3 +7,16 @@ def about(request):
     """ A view to return the about page """
 
     return render(request, 'about/about.html')
+
+
+def contact(request):
+    """ Imported emailJS code here """
+
+    # I realized the code is still visible in chrome dev tools,
+    # since Javascript is client-side, but I keep this in order
+    #  to show that I understand this at least, and for further development.
+    # I aim to use django email instead, similar to the checkout implementation
+
+    email_code = 'user_SNJGyOEpTaT5O7pC1cJUM'
+
+    return render(request, 'about/contact.html', {'email_code': email_code})

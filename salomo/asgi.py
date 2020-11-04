080e4ca7023fad3fd497793e9c06b23e56f07558
Voggastur <v3x0r_x014@hotmail.com>
2020-11-04 13:47:59 +0000

    add salomo.settings to wsgi and asgi.py

--- salomo/asgi.py
+++ salomo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for salomo project.
+ASGI config for boutique project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- salomo/wsgi.py
+++ salomo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for salomo project.
+WSGI config for boutique project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 

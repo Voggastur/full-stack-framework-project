38fc3d5dde9e0e4d453efbe2b622fc5b4a4c6b40
Voggastur <v3x0r_x014@hotmail.com>
2020-11-04 13:48:27 +0000

    add MEDIA_URL static reference

--- salomo/urls.py
+++ salomo/urls.py
@@ -1,4 +1,5 @@
 """boutique URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -12,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +27,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('contact/', include('about.urls')),
     path('about/', include('about.urls')),
     path('faq/', include('faq.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

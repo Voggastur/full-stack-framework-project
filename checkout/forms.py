f686ad2135f566eb2b3b0f9c8458f50cb0b089bb
Voggastur <v3x0r_x014@hotmail.com>
2020-11-04 12:45:08 +0000

    changed back street_adress2 to form and order model

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,7 +6,7 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
-                  'street_address',
+                  'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
                   'county',)
 
@@ -22,7 +22,8 @@ class OrderForm(forms.ModelForm):
             'phone_number': 'Phone Number',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
-            'street_address': 'Street Address',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
             'county': 'County, State or Locality',
         }
 

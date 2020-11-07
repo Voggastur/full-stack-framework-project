dcf226dbf4ff8ac3e89fffa237b1f17a24792fcf
Voggastur <v3x0r_x014@hotmail.com>
2020-11-07 22:08:25 +0000

    restored salomo.settings.STATICFILES_LOCATION

--- salomo/settings.py
+++ salomo/settings.py
@@ -151,9 +151,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_LOCATION = 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Cache control
